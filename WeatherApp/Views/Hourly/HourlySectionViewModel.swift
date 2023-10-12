03b9b021d49708c97242502c451f63f16c0f22e4
Egor Kruglov <yegorkruglov@gmail.com>
2023-10-12 18:41:55 +0300

    horly section number of hour changed to 12

--- WeatherApp/Views/Hourly/HourlySectionViewModel.swift
+++ WeatherApp/Views/Hourly/HourlySectionViewModel.swift
@@ -25,11 +25,35 @@ class HourlySectionViewModel: HourlySectionViewModelProtocol {
         hoursData.count
     }
     
-    private var hoursData: [Hour] { weatherData.value?.forecast.forecastday.first?.hour ?? [] }
+    private var hoursData: [Hour] { getHoursData() }
     
     private(set) var weatherData = Bindable<Weather?> (value: nil)
 
     required init(weatherData: Weather) {
         self.weatherData.value = weatherData
+    }
+    
+    private func getHoursData() -> [Hour] {
+        var hoursData: [Hour] = []
+        let currentDate = Date()
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        guard let allDays = weatherData.value?.forecast.forecastday else { return [] }
+        
+        var allHoursData = allDays.flatMap { $0.hour }
+        allHoursData.removeAll { hour in
+            guard let hourDate = dateFormatter.date(from: hour.time) else { return false }
+            
+            return hourDate < currentDate
+        }
+        
+        for index in 0...11 {
+            hoursData.append(allHoursData[index])
+        }
+        
+        
+        return hoursData
     }
 }
