24fe214f7d0e50c071542d022d397f3d4f0823c5
Egor Kruglov <yegorkruglov@gmail.com>
2023-10-13 14:46:27 +0300

    locationc TVC started

---
+++ WeatherApp/Viewcontrollers/LocationsTableViewController/LocationsTableViewControllerViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  LocationsTableViewControllerViewModel.swift
+//  WeatherApp
+//
+//  Created by Egor Kruglov on 13.10.2023.
+//
+
+import Foundation
+
+protocol LocationsTableViewControllerViewModelProtocol {
+    
+}
+
+final class LocationsTableViewControllerViewModel: LocationsTableViewControllerViewModelProtocol {
+    
+}
