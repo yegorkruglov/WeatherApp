18559be80fe59ee5decec424ffd6c68a30e8b112
Egor Kruglov <yegorkruglov@gmail.com>
2025-05-13 11:24:19 +0300

    Initial Commit

---
+++ WeatherApp/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Egor Kruglov on 13.05.2025.
+//
+
+import UIKit
+
+final class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .red
+    }
+
+
+}
+
